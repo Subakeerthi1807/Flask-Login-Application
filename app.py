1e1a0f8cae9a5c3a03814986b5635e86baf6f5fb
Subakeerthi <keertheeranjith07@gmail.com>
2026-09-10 09:58:59 +0530

    Initial Flask login application

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    if username == "Keerthi" and password == "1234":
+        return "Login Successful! 🎉"
+
+    else:
+        return "Invalid Username or Password ❌"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
